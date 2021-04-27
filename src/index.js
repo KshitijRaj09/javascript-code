6a220b5f1b3db018d47ceb1497c929376e4ea926
Kshitij Raj <59702912+KshitijRaj09@users.noreply.github.com>
2021-04-28 00:59:40 +0530

    array methods

--- src/index.js
+++ src/index.js
@@ -1,94 +1,152 @@
-import "./styles.css";
+// import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
+// // document.getElementById("app").innerHTML = `
+// // <h1>Hello Vanilla!</h1>
+// // <div>
+// //   We use the same configuration as Parcel to bundle this sandbox, you can find more
+// //   info about Parcel
+// //   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// // </div>
+// // `;
 
-const div = document.querySelectorAll("div");
+// const div = document.querySelectorAll("div");
 
-div.forEach((d) => {
-  d.addEventListener("click", clickHandler);
-});
+// div.forEach((d) => {
+//   d.addEventListener("click", clickHandler);
+// });
 
-function clickHandler(event) {
-  let element = event.target;
-  let p;
-  div.forEach((d) => {
-    if (d === element) {
-      // p = document.createElement("p");
-      // p.innerText = `P tag inside ${event.target.innerText}`;
-      // event.target.appendChild(p);
+// function clickHandler(event) {
+//   let element = event.target;
+//   let p;
+//   div.forEach((d) => {
+//     if (d === element) {
+//       // p = document.createElement("p");
+//       // p.innerText = `P tag inside ${event.target.innerText}`;
+//       // event.target.appendChild(p);
 
-      d.classList.add("addClass");
-    } else {
-      // let parent = p.parentNode;
-      // parent.remove(p);
-      d.classList.remove("addClass");
-    }
-  });
-}
-
-// function maxChar(str) {
-//   let maxChar = {};
-//   let max = 0;
-//   let maxValue = "";
-//   for (let characters of str) {
-//     if (!maxChar[characters]) {
-//       maxChar[characters] = 1;
+//       d.classList.add("addClass");
 //     } else {
-//       maxChar[characters] += 1;
+//       // let parent = p.parentNode;
+//       // parent.remove(p);
+//       d.classList.remove("addClass");
 //     }
-//   }
-//   console.log(maxChar);
-//   for (let char in maxChar) {
-//     if (max < maxChar[char]) {
-//       max = maxChar[char];
-//       maxValue = char;
-//     }
-//   }
-//   let arr = [maxValue, max];
-//   return arr;
+//   });
 // }
 
-// console.log(maxChar("abcccccccd"));
-// console.log(maxChar("apple 1231111"));
+// // function maxChar(str) {
+// //   let maxChar = {};
+// //   let max = 0;
+// //   let maxValue = "";
+// //   for (let characters of str) {
+// //     if (!maxChar[characters]) {
+// //       maxChar[characters] = 1;
+// //     } else {
+// //       maxChar[characters] += 1;
+// //     }
+// //   }
+// //   console.log(maxChar);
+// //   for (let char in maxChar) {
+// //     if (max < maxChar[char]) {
+// //       max = maxChar[char];
+// //       maxValue = char;
+// //     }
+// //   }
+// //   let arr = [maxValue, max];
+// //   return arr;
+// // }
 
-// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
-// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
-// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
-// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
-// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+// // console.log(maxChar("abcccccccd"));
+// // console.log(maxChar("apple 1231111"));
 
-// function chunk(arr, size) {
-//   let chunked = [];
+// // chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-//   for (let a of arr) {
-//     let lastElement = chunked[chunked.length - 1];
-//     if (!lastElement || lastElement.length === 2) {
-//       chunked.push([a]);
-//     } else chunked.push(a);
+// // function chunk(arr, size) {
+// //   let chunked = [];
+
+// //   for (let a of arr) {
+// //     let lastElement = chunked[chunked.length - 1];
+// //     if (!lastElement || lastElement.length === 2) {
+// //       chunked.push([a]);
+// //     } else chunked.push(a);
+// //   }
+// //   return chunked;
+// // }
+
+// // console.log(chunk([1, 3, 2, 5], 2));
+
+// const arr = [1, 4, 2, 7, 6];
+
+// //let a1 = arr.splice(-1,0, 8);
+
+// const arr2 = ["Hello", "World"];
+
+// const arr3 = [...arr2, ...arr]; //spread operator
+
+// const arr4 = arr2.concat(arr);
+// // arr2.push({ name: "Kshitij" }); //We can push items in constant array but we cannot initilize entry new values
+
+// // arr2.splice(0);
+
+// console.log(arr4);
+
+// let arr = [{
+//   name:'Kshitij',
+//   age: 25,
+// },
+// {
+//   name:'Raj',
+//   age:'22'},
+//   {
+//     name:'Kamal',
+//     age:'24'
 //   }
-//   return chunked;
-// }
+// ]
 
-// console.log(chunk([1, 3, 2, 5], 2));
+// let arr2 = [{
+//   name:'Kshitij',
+//   age: 25,
+// },
+// {
+//   name:'Raj',
+//   age:'22'},
+//   {
+//     name:'Kamal',
+//     age:'24'
+//   }
+// ]
 
-const arr = [1, 4, 2, 7, 6];
+// let newArr = arr.find(ar=>{
+//   return ar.name==="Raj"
+// })
 
-//let a1 = arr.splice(-1,0, 8);
+// console.log(newArr)
 
-const arr2 = ["Hello", "World"];
+// newArr.name="Singh"
 
-const arr3 = [...arr2, ...arr]; //spread operator
+// //console.log(arr);
 
-const arr4 = arr2.concat(arr);
-// arr2.push({ name: "Kshitij" }); //We can push items in constant array but we cannot initilize entry new values
+// let newMap = arr.map(ar=>{
+//   return ar
+// })
 
-// arr2.splice(0);
+// console.log(newMap)
 
-console.log(arr4);
+// newMap.name="Singh"
+
+// console.log(arr2)
+
+const employees = {
+  boss: "Michael",
+  secretary: "Pam",
+  sales: "Jim",
+  accountant: "Oscar"
+};
+
+// Get the keys of the object
+const keys = Object.entries(employees);
+
+console.log(keys);
